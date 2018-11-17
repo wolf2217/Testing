8a5fe171803cc6f28f07f82f12d106efd29809a1
Jason Caswell <jason.caswell@hotmail.com>
2018-11-16 20:57:41 -0700

    Second commit

--- Testing/Form1.vb
+++ Testing/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim lbl As New Label
+        Dim tmr As New Timer
+        lbl.Text = DateTime.Now.ToLongTimeString()
 
+    End Sub
 End Class
